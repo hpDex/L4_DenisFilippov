615771808558e24b656f1e10d50b8560078539cb
Denix <denixx@live.ru>
2020-07-27 20:11:28 +0300

    Вот моя попытка 2 опубликовать на GitHub

--- L4_DenisFilippov/main.swift
+++ L4_DenisFilippov/main.swift
@@ -8,5 +8,61 @@
 
 import Foundation
 
-print("Hello, World!")
+enum clickStart {
+    case start, stop
+}
 
+
+enum Trunk {
+    case empty, full
+}
+
+class someCar {
+    var brand : String
+    var model : String
+      init(brand: String, model:String) {
+          self.brand = brand
+          self.model = model
+         
+      }
+}
+  
+  
+class someTruck:someCar {
+
+    var BootState : Trunk
+    init(brand: String, model:String, BootState : Trunk) {
+        self.BootState = BootState
+        super.init(brand: brand, model: model)
+    }
+    func emptyTrunck() {
+        if BootState == .empty{
+        print ("\(brand)\(model) trunck is empty")
+        } else{ print ("\(brand)\(model) trunck is full")}
+    }
+    
+}
+   
+    class sportCar: someCar {
+        var moreFast: clickStart
+        init(brand: String, model:String, moreFast: clickStart) {
+            self.moreFast = moreFast
+            super.init(brand: brand, model: model)
+        }
+        
+        func moreFastOn() {
+            if moreFast == .start{
+                print("Спорт режим включен")
+            }else{
+                print("Комфортный режим включен")
+            }
+        }
+    }
+    
+let GAZ = someTruck(brand: "Газ", model: "Бычок", BootState: .empty)
+GAZ.emptyTrunck()
+let Bugatti = sportCar(brand: "Бугатти", model: "Вэйрон", moreFast: .start)
+Bugatti.moreFastOn()
+
+
+    
